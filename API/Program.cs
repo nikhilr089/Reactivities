69175ff1d2b8fd615a0e31e59d650d238e61b52c
ravi nikhil <nikhilr089@outlook.com>
2025-09-03 19:15:11 +0530

    ts changes and program changes

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
+builder.Services.AddCors(); //To Access Cross platform...
 
 var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001", "https://localhost:3001"));
--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -1,12 +1,12 @@
 type Activity = {
-  id: string
-  title: string
-  description: string
-  category: string
-  city: string
-  venue: string
-  isCancelled: boolean
-  date: string
-  latitude: number
-  longitude: number
+    id: string
+    title: string
+    description: string
+    category: string
+    city: string
+    venue: string
+    isCancelled: boolean
+    date: string
+    latitude: number
+    longitude: number
 }
